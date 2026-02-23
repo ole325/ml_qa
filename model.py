4fe5896dc5dbf03cc790a5834a90ba44001f1831
Oleg <olebmw@ya.ru>
2026-02-24 00:57:57 +0300

    frizz

--- model.py
+++ model.py
@@ -7,5 +7,9 @@ class SimpleNet(nn.Module):
         super().__init__()
         self.linear = nn.Linear(10, 1)
 
+        # Замораживаем веса
+        for p in self.parameters():
+            p.requires_grad = False
+
     def forward(self, x):
         return self.linear(x)
